7e1ad6af251b7c2956e9ac7d672663b1fd2c336f
EK-Gras <63175807+EK-Gras@users.noreply.github.com>
2026-07-09 19:47:26 -0400

    Commented Dashboard Code

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -12,13 +12,29 @@ function Dashboard() {
     navigate("/"); // back to the login page
   }
 
+  // Array stores PGx data as strings-- once we integrate with the backend we'll push data in here iteratively but for now it just has static junk data
   const Data = [
     ["CYP2C19", "7/9/2026", "Manual Entry", "*2/*2", "Poor Metabolizer"],
     ["ENTRY 2", "7/9/2026", "Manual Entry", "*2/*2", "Poor Metabolizer"],
     ["ENTRY 3", "7/9/2026", "Manual Entry", "*2/*2", "Poor Metabolizer"]
   ];
 
+  // Takes a PGx data point and formats it into a card
+  function DisplayDataCard(geneName, timestamp, source, genotype, phenotype) {
+    return (
+      <div className="card_data">
+        <h2 className="card_header">{geneName}</h2>
+        <p className="card_timestamp">{timestamp} - {source}</p>
+        <break></break>
+        <p>Phenotype: {phenotype}</p>
+        <p>Genotype: {genotype}</p>
+      </div>
+    );
+  }
+
+  // Takes all PGx data points and creates a set of cards
   function DisplayDataCardList() {
+    // Map each data point into a Data Card and return an array of those cards
     const DataCards = Data.map((entry) => DisplayDataCard(entry[0], entry[1], entry[2], entry[3], entry[4]))
     return(DataCards);
   }
@@ -42,18 +58,6 @@ function Dashboard() {
             </div>
           </div>
       </main>
-    </div>
-  );
-}
-
-function DisplayDataCard(geneName, timestamp, source, genotype, phenotype) {
-  return (
-    <div className="card_data">
-      <h2 className="card_header">{geneName}</h2>
-      <p className="card_timestamp">{timestamp} - {source}</p>
-      <break></break>
-      <p>Phenotype: {phenotype}</p>
-      <p>Genotype: {genotype}</p>
     </div>
   );
 }
